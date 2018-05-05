b4ee2163b9c7d11df43b5ed2b2ab771af41ab5d1
Krzysztof <krzysztof@augustowski.net>
2018-05-05 09:28:34 +0200

    Components & databinding: Local template references

--- components-and-databinding/src/app/components/cockpit/cockpit.component.ts
+++ components-and-databinding/src/app/components/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,19 +8,25 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output() blueprintCreated = new EventEmitter<{blueprintName: string, blueprintContent: string}>();
-  newServerName = '';
-  newServerContent = '';
+  // newServerName = '';
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onAddServer() {
-    this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+  onAddServer(serverName) {
+    this.serverCreated.emit({
+      serverName: serverName,
+      serverContent: this.serverContentInput.nativeElement.value
+    });
   }
 
-  onAddBlueprint() {
-    this.blueprintCreated.emit({blueprintName: this.newServerName, blueprintContent: this.newServerContent});
+  onAddBlueprint(serverName) {
+    this.blueprintCreated.emit({
+      blueprintName: serverName,
+      blueprintContent: this.serverContentInput.nativeElement.value
+    });
   }
 }
