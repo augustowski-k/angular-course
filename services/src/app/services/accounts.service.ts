4fda4bfbb2a2b138930d3c8ae8f223c943bfe5d6
Krzysztof <krzysztof@augustowski.net>
2018-05-15 21:00:34 +0200

    Services: introduction

---
+++ services/src/app/services/accounts.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { LoggingService } from './logging.service';
+
+@Injectable()
+export class AccountsService {
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
+
+  constructor(private logging: LoggingService) {}
+
+  addAccount(name: string, status: string) {
+    this.accounts.push({name: name, status: status});
+    this.logging.logStatusChange(status);
+  }
+  updateStatus(id: number, status: string) {
+    this.accounts[id].status = status;
+    this.logging.logStatusChange(status);
+  }
+}
